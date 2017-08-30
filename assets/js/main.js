bcb87579ed7baf938cc26a0a60b3e40d9b28038d
Marian Menschig <mmenschig@marinsoftware.com>
2017-08-30 16:22:21 -0700

    wrote api test call

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,18 @@
-//Insert main js functions here
+
+
+const BASE_URL = ''
+
+// IIFE to test API CALL
+(function testCall() {
+  $.ajax({
+    url: null,
+    dataType: 'json',
+    method: 'GET',
+    success: function(res) {
+      console.log(res);
+    },
+    error: function(e) {
+      console.log(e);
+    }
+  })
+})();
